af8a941f1ae2d8be47e51b616689dc83eb5ce189
Rahul Kumar <2470251@kiit.ac.in>
2025-07-07 02:46:12 +0530

    added utility functions supporting unit tests

---
+++ utils/utils.cc
@@ -0,0 +1,17 @@
+#include "utils.h"
+#include <cstdint>
+
+void badRequest(std::function<void(const drogon::HttpResponsePtr &)> &&callback, std::string err, drogon::HttpStatusCode code)
+{
+    Json::Value ret{};
+    ret["error"] = err;
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
+    resp->setStatusCode(code);
+    callback(resp);
+}
+
+Json::Value makeErrResp(std::string err) {
+    Json::Value ret{};
+    ret["error"] = err;
+    return ret;
+}
